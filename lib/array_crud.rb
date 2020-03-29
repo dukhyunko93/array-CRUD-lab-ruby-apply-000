cd182cb26a2bf04b18ea7ed289be1754339fbf5d
Duke Ko <dukhyunko93@gmail.com>
2020-03-29 19:24:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,31 +32,4 @@ end
 
 def retrieve_last_element_from_array(array)
   array[-1]
-=======
-
-end
-
-def add_element_to_start_of_array(array, element)
-
-end
-
-def remove_element_from_end_of_array(array)
-
-end
-
-def remove_element_from_start_of_array(array)
-
-end
-
-def retrieve_element_from_index(array, index_number)
-
-end
-
-def retrieve_first_element_from_array(array)
-
-end
-
-def retrieve_last_element_from_array(array)
-
->>>>>>> 88cccb872dd2ebcd08367a9a877bc49d02d7e77a
 end
